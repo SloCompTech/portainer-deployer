665bf3b9a2a3df7edfd7aa8159075ef1994c3c21
Martin <martin.dagarin@gmail.com>
2020-04-26 00:19:43 +0200

    Fixed authentication

--- auth.js
+++ auth.js
@@ -12,19 +12,25 @@ const init = async function (req, res, next) {
 
   try {
     const auth = (req.headers['authorization']) ? req.headers['authorization'].split(' ') : null;
-    if (!auth) // No autnetication
-      return next(); // Still continue, because here we only extract data
     
-    const method = auth[0], val = auth[1];
-    if (method == 'Bearer') { // Token
-      req.token = val;
-      req.user = jwt.decode(val);
-    } else if (method == 'Basic') { // Username & password
-      const raw = Buffer.from(val, 'base64').toString('utf8');
-      const credentials = raw.split(':');
-      const token = await portainer.login(credentials[0] || process.env.PORTAINER_USER, credentials[1] || process.env.PORTAINER_PASS);
-      req.token = token;
-      req.user = jwt.decode(token);
+    if (!auth) { // No authentication data in request
+      if (process.env.PORTAINER_USER) { // Use credentials from environment variables
+        const token = await portainer.login(process.env.PORTAINER_USER, process.env.PORTAINER_PASS);
+        req.token = token;
+        req.user = jwt.decode(token);
+      }
+    } else {
+      const method = auth[0], val = auth[1];
+      if (method == 'Bearer') { // Token
+        req.token = val;
+        req.user = jwt.decode(val);
+      } else if (method == 'Basic') { // Username & password
+        const raw = Buffer.from(val, 'base64').toString('utf8');
+        const credentials = raw.split(':');
+        const token = await portainer.login(credentials[0] || process.env.PORTAINER_USER, (credentials.length > 1 ? credentials[1] : null) || process.env.PORTAINER_PASS);
+        req.token = token;
+        req.user = jwt.decode(token);
+      }
     }
   } catch (e) {
     console.log(e);
